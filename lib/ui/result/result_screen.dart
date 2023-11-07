368948f157b2841def083fac3acd1b63c2e3746b
ldphat99 <ldphat99@gmail.com>
2023-11-07 22:40:39 +0700

    UPDATE: handle show result

--- lib/ui/result/result_screen.dart
+++ lib/ui/result/result_screen.dart
@@ -4,7 +4,10 @@ import 'package:ct484_final_project/configs/themes/theme.dart';
 import 'package:ct484_final_project/ui/menu/menu_screen.dart';
 
 class ResultScreen extends StatelessWidget {
-  const ResultScreen({super.key});
+  final int correctQuestion;
+  final int totalQuestions;
+  const ResultScreen(
+      {super.key, required this.correctQuestion, required this.totalQuestions});
 
   @override
   Widget build(BuildContext context) {
@@ -16,42 +19,75 @@ class ResultScreen extends StatelessWidget {
         child: Stack(
           children: [
             Positioned(
-              top: 180,
+              top: 100,
               left: 20,
               right: 20,
               bottom: 150,
               child: Container(
                 width: MediaQuery.of(context).size.width,
-                height: 271,
+                height: 270,
                 decoration: BoxDecoration(
                   color: softblueColor,
                   border: Border.all(color: blackColor, width: 4.0),
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderRadius: const BorderRadius.all(Radius.circular(20.0)),
                 ),
                 child: Center(
                   child: Column(
                     children: [
-                      SizedBox(
-                        height: 90,
-                      ),
-                      Text(
-                        "Your final score is",
-                        style: mediaumTextStyle.copyWith(fontSize: 26),
+                      const SizedBox(
+                        height: 50,
                       ),
                       Container(
-                        width: 200.0,
-                        height: 150.0,
-                        margin: EdgeInsets.all(80.0),
+                        width: 220.0,
+                        height: 220.0,
+                        margin: const EdgeInsets.all(60.0),
                         decoration: BoxDecoration(
-                            border: Border.all(color: whiteColor, width: 4.0),
-                            color: redColor,
-                            shape: BoxShape.circle),
-                        child: Center(
-                          child: Text(
-                            "60",
-                            style: mediaumTextStyle.copyWith(
-                                fontSize: 72, color: whiteColor),
+                          border: Border.all(color: whiteColor, width: 4.0),
+                          color: redColor,
+                          shape: BoxShape.circle,
+                        ),
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(
+                                "${(correctQuestion / totalQuestions * 100).toInt()}",
+                                style: mediaumTextStyle.copyWith(
+                                  fontSize: 72,
+                                  color: whiteColor,
+                                ),
+                              ),
+                              Text(
+                                "Your score",
+                                style: mediaumTextStyle.copyWith(
+                                  fontSize: 18,
+                                  color: whiteColor,
+                                ),
+                              ),
+                            ],
                           ),
+                        ),
+                      ),
+                      Text(
+                        'Total questions: $totalQuestions',
+                        style: mediaumTextStyle.copyWith(
+                          fontSize: 22,
+                          color: blackColor,
+                        ),
+                      ),
+                      Text(
+                        'Correct questions: $correctQuestion',
+                        style: mediaumTextStyle.copyWith(
+                          fontSize: 22,
+                          color: blackColor,
+                        ),
+                      ),
+                      Text(
+                        'Wrong questions: ${totalQuestions - correctQuestion}',
+                        style: mediaumTextStyle.copyWith(
+                          fontSize: 22,
+                          color: blackColor,
                         ),
                       ),
                     ],
@@ -60,11 +96,11 @@ class ResultScreen extends StatelessWidget {
               ),
             ),
             Positioned(
-              top: 100,
+              top: 40,
               left: 100,
               height: 150,
               width: 206,
-              child: Container(
+              child: SizedBox(
                 width: 120,
                 height: 120,
                 child: Image.asset(
@@ -78,19 +114,21 @@ class ResultScreen extends StatelessWidget {
               left: 100,
               height: 50,
               width: 206,
-              child: Container(
+              child: SizedBox(
                 width: 220,
                 height: 50,
                 child: ElevatedButton(
                   onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => MenuScreen()));
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const MenuScreen()));
                   },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: blueColor,
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(22),
-                      side: BorderSide(color: Colors.white, width: 2.0),
+                      side: const BorderSide(color: Colors.white, width: 2.0),
                     ),
                   ),
                   child: Text(
@@ -107,3 +145,4 @@ class ResultScreen extends StatelessWidget {
     );
   }
 }
+
