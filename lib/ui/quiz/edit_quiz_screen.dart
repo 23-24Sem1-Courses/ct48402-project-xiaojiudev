a7175bef58c146152148d36f70dff4f1660d275f
ldphat99 <ldphat99@gmail.com>
2023-11-08 23:55:26 +0700

    ADD edit quiz screen

---
+++ lib/ui/quiz/edit_quiz_screen.dart
@@ -0,0 +1,216 @@
+import 'package:ct484_final_project/configs/themes/theme.dart';
+import 'package:ct484_final_project/utils/app_logger.dart';
+import 'package:flutter/material.dart';
+
+class EditQuizScreen extends StatefulWidget {
+  const EditQuizScreen({super.key});
+
+  @override
+  State<EditQuizScreen> createState() => _EditQuizScreenState();
+}
+
+class _EditQuizScreenState extends State<EditQuizScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _imageUrlController = TextEditingController();
+  final _imageUrlFocusNode = FocusNode();
+
+  var _isLoading = false;
+
+  bool _isValidImageUrl(String value) {
+    return (value.startsWith('http') || value.startsWith('https')) &&
+        (value.endsWith('.png') ||
+            value.endsWith('.jpg') ||
+            value.endsWith('jpeg'));
+  }
+
+  @override
+  void initState() {
+    _imageUrlFocusNode.addListener(() {
+      if (!_imageUrlFocusNode.hasFocus) {
+        if (!_isValidImageUrl(_imageUrlController.text)) {
+          return;
+        }
+        setState(() {});
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _imageUrlController.dispose();
+    _imageUrlFocusNode.dispose();
+    super.dispose();
+  }
+
+  Future<void> _saveForm() async {
+    final isValid = _formKey.currentState!.validate();
+
+    if (!isValid) {
+      return;
+    }
+
+    _formKey.currentState!.save();
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {} catch (error) {
+      if (mounted) {
+        AppLogger.error('Something went wrong.');
+      }
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
+
+    if (mounted) {
+      Navigator.of(context).pop();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Edit Quiz'),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.save),
+              onPressed: _saveForm,
+            ),
+          ],
+		  backgroundColor: softblueColor,
+        ),
+        
+        body: _isLoading
+            ? const Center(child: CircularProgressIndicator())
+            : Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Form(
+                  key: _formKey,
+                  child: ListView(
+                    children: [
+                      buildTitleField(),
+                      buildDescriptionField(),
+                      buildQuizPreview(),
+                      buildTimeField(),
+                    ],
+                  ),
+                ),
+              ));
+  }
+
+  TextFormField buildTitleField() {
+    return TextFormField(
+      initialValue: '',
+      decoration: const InputDecoration(labelText: 'Title'),
+      textInputAction: TextInputAction.next,
+      autofocus: true,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Please provide a value';
+        }
+        return null;
+      },
+      onSaved: (value) {},
+    );
+  }
+
+  TextFormField buildDescriptionField() {
+    return TextFormField(
+      initialValue: '',
+      decoration: const InputDecoration(labelText: 'Description'),
+      maxLines: 3,
+      keyboardType: TextInputType.multiline,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Please enter a description.';
+        }
+        if (value.length < 10) {
+          return 'Should be at least 10 characters long.';
+        }
+
+        return null;
+      },
+      onSaved: (value) {},
+    );
+  }
+
+  buildQuizPreview() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: <Widget>[
+        Container(
+          width: 100,
+          height: 100,
+          margin: const EdgeInsets.only(
+            top: 8,
+            right: 10,
+          ),
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Colors.grey,
+            ),
+          ),
+          child: _imageUrlController.text.isEmpty
+              ? const Text('Enter a URL')
+              : FittedBox(
+                  child: Image.network(
+                    _imageUrlController.text,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+        ),
+        Expanded(
+          child: buildImageURLField(),
+        ),
+      ],
+    );
+  }
+
+  TextFormField buildTimeField() {
+    return TextFormField(
+      initialValue: 0.toString(),
+      decoration: const InputDecoration(labelText: 'Time'),
+      textInputAction: TextInputAction.next,
+      keyboardType: TextInputType.number,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Please enter a time in seconds.';
+        }
+        if (double.tryParse(value) == null) {
+          return 'Please enter a valid number';
+        }
+        if (double.parse(value) <= 0) {
+          return 'Please enter a number greater than zero';
+        }
+        return null;
+      },
+      onSaved: (value) {},
+    );
+  }
+
+  buildImageURLField() {
+    return TextFormField(
+      decoration: const InputDecoration(labelText: 'Image URL'),
+      keyboardType: TextInputType.url,
+      textInputAction: TextInputAction.done,
+      controller: _imageUrlController,
+      focusNode: _imageUrlFocusNode,
+      onFieldSubmitted: (value) => _saveForm(),
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Please enter an image URL';
+        }
+        if (!_isValidImageUrl(value)) {
+          return 'Please enter a valid image URL';
+        }
+        return null;
+      },
+      onSaved: (value) {},
+    );
+  }
+}
