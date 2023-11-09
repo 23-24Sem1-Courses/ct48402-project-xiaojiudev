ceaf5981aed184372362d40549966c5f5393caea
ldphat99 <ldphat99@gmail.com>
2023-11-09 17:47:03 +0700

    UPDATE: delete quiz

--- lib/ui/quiz/user_quiz_screen.dart
+++ lib/ui/quiz/user_quiz_screen.dart
@@ -1,12 +1,12 @@
-import 'package:ct484_final_project/ui/menu/menu_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:loading_indicator/loading_indicator.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
+import 'package:ct484_final_project/models/quiz.dart';
 import 'package:ct484_final_project/utils/app_logger.dart';
 import 'package:ct484_final_project/configs/themes/theme.dart';
-import 'package:ct484_final_project/services/quiz_service.dart';
 import 'package:ct484_final_project/ui/quiz/edit_quiz_screen.dart';
+import 'package:ct484_final_project/services/firebase_quiz_service.dart';
 
 class UserQuizScreen extends StatefulWidget {
   const UserQuizScreen({super.key});
@@ -17,6 +17,8 @@ class UserQuizScreen extends StatefulWidget {
 
 class _UserQuizScreenState extends State<UserQuizScreen> {
   final FirebaseQuizService quizService = FirebaseQuizService();
+  final GlobalKey<RefreshIndicatorState> _refreshKey =
+      GlobalKey<RefreshIndicatorState>();
 
   @override
   Widget build(BuildContext context) {
@@ -27,62 +29,74 @@ class _UserQuizScreenState extends State<UserQuizScreen> {
           buildAddButton(context),
         ],
         backgroundColor: softblueColor,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back),
+        ),
       ),
-      body: FutureBuilder(
-        future: quizService.fetchAllQuizzes(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: SizedBox(
-                width: 60,
-                height: 60,
-                child: LoadingIndicator(
-                  indicatorType: Indicator.ballSpinFadeLoader,
-                  strokeWidth: 1,
-                ),
-              ),
-            );
-          } else if (snapshot.hasError) {
-            AppLogger.info('${snapshot.error}');
-
-            return const Center(child: Text('Something went wrong'));
-          } else {
-            final quizzes = snapshot.data;
-
-            return ListView.builder(
-              itemCount: quizzes!.length,
-              itemBuilder: (context, index) {
-                final quiz = quizzes[index];
-
-                return Padding(
-                  padding: const EdgeInsets.only(top: 20),
-                  child: ListTile(
-                    leading: CachedNetworkImage(
-                      imageUrl: quiz.imageUrl!,
-                      placeholder: (context, url) => const LoadingIndicator(
-                        indicatorType: Indicator.ballSpinFadeLoader,
-                        strokeWidth: 1,
-                      ),
-                      errorWidget: (context, url, error) =>
-                          const Icon(Icons.error),
-                      width: 65,
-                    ),
-                    title: Text(quiz.title),
-                    trailing: SizedBox(
-                      width: 100,
-                      child: Row(
-                        children: <Widget>[
-                          buildEditButton(context),
-                          buildDeleteButton(context),
-                        ],
-                      ),
-                    ),
-                  ),
-                );
-              },
-            );
-          }
+      body: RefreshIndicator(
+        key: _refreshKey,
+        onRefresh: () async {
+          setState(() {});
         },
+        child: FutureBuilder(
+          future: quizService.fetchAllQuizzes(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: SizedBox(
+                  width: 60,
+                  height: 60,
+                  child: LoadingIndicator(
+                    indicatorType: Indicator.ballSpinFadeLoader,
+                    strokeWidth: 1,
+                  ),
+                ),
+              );
+            } else if (snapshot.hasError) {
+              AppLogger.info('${snapshot.error}');
+
+              return const Center(child: Text('Something went wrong'));
+            } else {
+              final quizzes = snapshot.data;
+
+              return ListView.builder(
+                itemCount: quizzes!.length,
+                itemBuilder: (context, index) {
+                  final quiz = quizzes[index];
+
+                  return Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: ListTile(
+                      leading: CachedNetworkImage(
+                        imageUrl: quiz.imageUrl!,
+                        placeholder: (context, url) => const LoadingIndicator(
+                          indicatorType: Indicator.ballSpinFadeLoader,
+                          strokeWidth: 1,
+                        ),
+                        errorWidget: (context, url, error) =>
+                            const Icon(Icons.error),
+                        width: 65,
+                      ),
+                      title: Text(quiz.title),
+                      trailing: SizedBox(
+                        width: 100,
+                        child: Row(
+                          children: <Widget>[
+                            buildEditButton(context),
+                            buildDeleteButton(context, quiz),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+          },
+        ),
       ),
     );
   }
@@ -91,7 +105,7 @@ class _UserQuizScreenState extends State<UserQuizScreen> {
     return IconButton(
       icon: const Icon(Icons.add),
       onPressed: () {
-        Navigator.pushReplacement(
+        Navigator.push(
             context, MaterialPageRoute(builder: (context) => EditQuizScreen()));
       },
     );
@@ -107,24 +121,51 @@ class _UserQuizScreenState extends State<UserQuizScreen> {
     );
   }
 
-  Widget buildDeleteButton(BuildContext context) {
+  Widget buildDeleteButton(BuildContext context, QuizCourse quiz) {
     return IconButton(
       icon: const Icon(Icons.delete),
       onPressed: () {
-        AppLogger.info('Delete quiz');
-
-        ScaffoldMessenger.of(context)
-          ..hideCurrentSnackBar()
-          ..showSnackBar(
-            const SnackBar(
-              content: Text(
-                'Quiz deleted',
-                textAlign: TextAlign.center,
-              ),
-            ),
-          );
+        showDeleteConfirmationDialog(context, quiz);
       },
       color: Theme.of(context).colorScheme.error,
+    );
+  }
+
+  void showDeleteConfirmationDialog(BuildContext context, QuizCourse quiz) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Delete Quiz'),
+          content: const Text('Are you sure you want to delete this quiz?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () async {
+                await quizService.deleteQuiz(quiz.id);
+                Navigator.of(context).pop();
+
+                _refreshKey.currentState?.show();
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text(
+                      'Quiz deleted',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                );
+              },
+              child: const Text('Delete'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
