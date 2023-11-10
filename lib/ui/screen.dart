df0619db67534bed0b4c4a5649c21224c3d5c7bf
ldphat99 <ldphat99@gmail.com>
2023-11-10 21:42:58 +0700

    ADD: export all screen

---
+++ lib/ui/screen.dart
@@ -0,0 +1,11 @@
+export 'detail/detail_screen.dart';
+
+export 'menu/menu_screen.dart';
+
+export 'quiz/quiz_screen.dart';
+export 'quiz/edit_quiz_screen.dart';
+export 'quiz/user_quiz_screen.dart';
+
+export 'result/result_screen.dart';
+
+export 'splash/splash_screen.dart';
