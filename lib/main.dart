edbc62e7d3570b9172bcfe4d04f0d80cd8d8fc50
ldphat99 <ldphat99@gmail.com>
2023-11-10 22:52:10 +0700

    UPDATE: quizProivder

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
-import 'package:ct484_final_project/models/quiz.dart';
+import 'package:ct484_final_project/ui/quiz/quiz_provider.dart';
+import 'package:provider/provider.dart';
 
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 import 'package:ct484_final_project/ui/screen.dart';
+import 'package:ct484_final_project/models/quiz.dart';
 import 'package:ct484_final_project/services/firebase_init_quiz_service.dart';
 
 void main() async {
@@ -24,55 +26,60 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Study Quiz Application',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-      home: const SplashScreen(),
-      routes: {
-        MenuScreen.routeName: (ctx) => const MenuScreen(),
-		UserQuizScreen.routeName: (ctx) => const UserQuizScreen(),
-        EditQuizScreen.routeName: (ctx) {
-          final quiz = ModalRoute.of(ctx)!.settings.arguments as QuizCourse?;
-          return EditQuizScreen(quiz);
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => QuizProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Study Quiz Application',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          useMaterial3: true,
+        ),
+        home: const SplashScreen(),
+        routes: {
+          MenuScreen.routeName: (ctx) => const MenuScreen(),
+          UserQuizScreen.routeName: (ctx) => const UserQuizScreen(),
+          EditQuizScreen.routeName: (ctx) {
+            final quiz = ModalRoute.of(ctx)!.settings.arguments as QuizCourse?;
+            return EditQuizScreen(quiz);
+          },
         },
-      },
-      onGenerateRoute: (settings) {
-        if (settings.name == QuizScreen.routeName) {
-          final quiz = settings.arguments as QuizCourse;
-          return MaterialPageRoute(
-            builder: (context) => QuizScreen(quiz: quiz),
-          );
-        }
+        onGenerateRoute: (settings) {
+          if (settings.name == QuizScreen.routeName) {
+            final quiz = settings.arguments as QuizCourse;
+            return MaterialPageRoute(
+              builder: (context) => QuizScreen(quiz: quiz),
+            );
+          }
 
-        if (settings.name == DetailScreen.routeName) {
-          final arguments = settings.arguments as Map<String, dynamic>;
-          final questions = arguments['questions'] as List<Question>;
-          final timeCountdown = arguments['timeCountdown'] as int;
+          if (settings.name == DetailScreen.routeName) {
+            final arguments = settings.arguments as Map<String, dynamic>;
+            final questions = arguments['questions'] as List<Question>;
+            final timeCountdown = arguments['timeCountdown'] as int;
 
-          return MaterialPageRoute(
-            builder: (context) => DetailScreen(
-              questions: questions,
-              timeCountdown: timeCountdown,
-            ),
-          );
-        }
+            return MaterialPageRoute(
+              builder: (context) => DetailScreen(
+                questions: questions,
+                timeCountdown: timeCountdown,
+              ),
+            );
+          }
 
-        if (settings.name == ResultScreen.routeName) {
-          final arguments = settings.arguments as Map<String, dynamic>;
-          final correctQuestion = arguments['correctQuestion'] as int;
-          final totalQuestions = arguments['totalQuestions'] as int;
+          if (settings.name == ResultScreen.routeName) {
+            final arguments = settings.arguments as Map<String, dynamic>;
+            final correctQuestion = arguments['correctQuestion'] as int;
+            final totalQuestions = arguments['totalQuestions'] as int;
 
-          return MaterialPageRoute(
-            builder: (context) => ResultScreen(
-              correctQuestion: correctQuestion,
-              totalQuestions: totalQuestions,
-            ),
-          );
-        }
-      },
+            return MaterialPageRoute(
+              builder: (context) => ResultScreen(
+                correctQuestion: correctQuestion,
+                totalQuestions: totalQuestions,
+              ),
+            );
+          }
+        },
+      ),
     );
   }
 }
