d9e9ad2e8c0b79631f497822b54f04c3383031df
ldphat99 <ldphat99@gmail.com>
2023-11-15 21:07:44 +0700

    UPDATE: optimizing with fromJSON, toJSON function

--- lib/services/firebase_quiz_service.dart
+++ lib/services/firebase_quiz_service.dart
@@ -11,31 +11,12 @@ class FirebaseQuizService {
 
   Future<void> saveQuiz(QuizCourse quiz) async {
     try {
-      final quizData = {
-		'id': quiz.id,
-        'title': quiz.title,
-        'image_url': quiz.imageUrl,
-        'Description': quiz.description,
-        'time_seconds': quiz.timeSeconds,
-        'questions': quiz.questions!.map((question) {
-          return {
-			'id': question.id,
-            'question': question.question,
-            'correct_answer': question.correctAnswer,
-            'answers': question.answers!.map((answer) {
-              return {
-                'identifier': answer.identifier,
-                'text': answer.text,
-              };
-            }).toList(),
-          };
-        }).toList(),
-      };
+      final quizData = quiz.toJson();
 
-	   await _firestore
-            .collection(FIREBASE_QUIZ_COLLECTION)
-            .doc(quiz.id)
-            .set(quizData);
+      await _firestore
+          .collection(FIREBASE_QUIZ_COLLECTION)
+          .doc(quiz.id)
+          .set(quizData);
     } catch (e) {
       AppLogger.error(e);
     }
@@ -60,28 +41,7 @@ class FirebaseQuizService {
       final quizzes = querySnapshot.docs.map((doc) {
         final data = doc.data();
 
-        // AppLogger.info(data);
-
-        return QuizCourse(
-          id: data['id'],
-          title: data['title'],
-          imageUrl: data['image_url'],
-          description: data['Description'],
-          timeSeconds: data['time_seconds'],
-          questions: (data['questions'] as List).map((questionData) {
-            return Question(
-              id: questionData['id'],
-              question: questionData['question'],
-              correctAnswer: questionData['correct_answer'],
-              answers: (questionData['answers'] as List).map((answerData) {
-                return Answer(
-                  identifier: answerData['identifier'],
-                  text: answerData['text'],
-                );
-              }).toList(),
-            );
-          }).toList(),
-        );
+        return QuizCourse.fromJson(data);
       }).toList();
 
       return quizzes;
